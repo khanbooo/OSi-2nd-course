e05f2f16df2ff340664cb7f2740f6833500465c4
khanbo <sharkosergey222004@gmail.com>
2024-05-13 16:07:24 +0700

    3.2 added errors handling and recursive directory deletion

--- task3/commands.c
+++ task3/commands.c
@@ -5,21 +5,41 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <errno.h>
+
+void buildPath(const char* path, const char* name, char* newDirectoryPath){
+    strcpy(newDirectoryPath, path);
+    if (path[strlen(path) - 1] != '/') {
+        strcat(newDirectoryPath, "/");
+    }
+    strcpy(newDirectoryPath + strlen(path) + (path[strlen(path) - 1] != '/'), name);
+}
 
 void printError(char* funcName){
-    printf("Unable to perform %s\n", funcName);
+    fprintf(stderr, "Unable to perform %s:\n%s\n", funcName, strerror(errno));
+}
+
+void myChmod(char* path, mode_t mode){
+    if (chmod(path, mode) == -1){
+        fprintf(stderr, "Unable to change mode to %s\n", path);
+        printError("myChmod");
+        return;
+    }
 }
 
 void myMkdir(char* path, mode_t mode){
-    if (mkdir(path, mode) == -1){
+    if (mkdir(path, 0777) == -1){
+        fprintf(stderr, "Unable to create directory at %s\n", path);
         printError("myMkdir");
         return;
     }
+    myChmod(path, mode);
 }
 
 void myLs(char* path){
     DIR* src = opendir(path);
     if (src == NULL){
+        fprintf(stderr, "Unable to open directory at %s\n", path);
         printError("myLs");
         return;
     }
@@ -29,8 +49,31 @@ void myLs(char* path){
     }
 }
 
+void myRm(char* path){
+    if (unlink(path) == -1){
+        fprintf(stderr, "Unable to delete file at %s\n", path);
+        printError("myRm");
+        return;
+    }
+}
+
 void myRmDir(char* path){
+    DIR* dir = opendir(path);
+    struct dirent* dirEntry;
+    while (dirEntry = readdir(dir)){
+        if (dirEntry->d_type == DT_REG){
+            char filePath[256];
+            buildPath(path, dirEntry->d_name, filePath);
+            myRm(filePath);
+        }
+        else if (dirEntry->d_type == DT_DIR && strcmp(dirEntry->d_name, ".") && strcmp(dirEntry->d_name, "..")){
+            char newSrc[256];
+            buildPath(path, dirEntry->d_name, newSrc);
+            myRmDir(newSrc);
+        }
+    }
     if (rmdir(path) == -1){
+        fprintf(stderr, "Unable to delete directory at %s\n", path);
         printError("myRmDir");
         return;
     }
@@ -38,23 +81,27 @@ void myRmDir(char* path){
 
 void myTouch(char* path, mode_t mode){
     printf("%o\n", mode);
-    if (creat(path, mode) == -1){
+    if (creat(path, 0777) == -1){
+        fprintf(stderr, "Unable to create file at %s\n", path);
         printError("myTouch");
         return;
     }
+    myChmod(path, mode);
 }
 
 void myCat(char* path){
     int fd = open(path, O_RDONLY);
     if (fd == -1){
+        fprintf(stderr, "Unable to open file at %s\n", path);
         printError("myCat");
         return;
     }
     char buffer[1024];
-    int counter = 1;
+    long counter = 1;
     while (counter > 0){
         counter = read(fd, buffer, 1024 * sizeof(unsigned char));
         if (counter == -1){
+            fprintf(stderr, "Error while reading from file\n");
             printError("myCat");
             close(fd);
             return;
@@ -64,20 +111,13 @@ void myCat(char* path){
             printf("%c", buffer[bWritten]);
             bWritten++;
         }
-        //printf("\n");
     }
     close(fd);
 }
 
-void myRm(char* path){
-    if (unlink(path) == -1){
-        printError("myRm");
-        return;
-    }
-}
-
-void mySymlink(char* target, char* linkpath){
-    if (symlink(target, linkpath) == -1){
+void mySymlink(char* target, char* linkPath){
+    if (symlink(target, linkPath) == -1){
+        fprintf(stderr, "Unable to create symbolic link to %s\n", target);
         printError("mySymlink");
         return;
     }
@@ -86,8 +126,9 @@ void mySymlink(char* target, char* linkpath){
 void myReadLink(char* path){
     int bufsize = PATH_MAX;
     char buffer[bufsize];
-    int nBytes = readlink(path, buffer, bufsize);
+    long nBytes = readlink(path, buffer, bufsize);
     if (nBytes == -1){
+        fprintf(stderr, "Unable to read link at %s\n", path);
         printError("myReadLink");
         return;
     }
@@ -101,8 +142,9 @@ void myReadLink(char* path){
 void myReadFileBySymlink(char* path){
     int bufsize = PATH_MAX;
     char buffer[bufsize];
-    int nBytes = readlink(path, buffer, bufsize);
+    long nBytes = readlink(path, buffer, bufsize);
     if (nBytes == -1){
+        fprintf(stderr, "Unable to read link at %s\n", path);
         printError("myReadFileBySymlink");
         return;
     }
@@ -111,6 +153,7 @@ void myReadFileBySymlink(char* path){
 
 void myLink(char* oldPath, char* newPath){
     if (link(oldPath, newPath) == -1){
+        fprintf(stderr, "Unable to create link to %s\n", oldPath);
         printError("myLink");
         return;
     }
@@ -121,20 +164,14 @@ void myLink(char* oldPath, char* newPath){
 void myStat(char* path){
     struct stat st;
     if (stat(path, &st) == -1){
+        fprintf(stderr, "Unable to get statistics of %s\n", path);
         printError("myStat");
         return;
     }
     printf("mode: %o\nnumber of hard links: %ld\n", st.st_mode&0777, st.st_nlink);
 }
 
-void myChmod(char* path, mode_t mode){
-    if (chmod(path, mode) == -1){
-        printError("myChmod");
-        return;
-    }
-}
-
-int main(int argc, char** argv[]){
+int main(int argc, char** argv){
     if (argc != 2 && argc != 3){
         printf("bad arguments\n");
         return 0;
